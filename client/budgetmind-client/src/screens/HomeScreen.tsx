46fce2e6312fc0850a23d18d8aae93e770dc92a9
Franco <francomartin.personal@gmail.com>
2021-12-26 17:54:23 -0300

    minimum design of Home

--- client/budgetmind-client/src/screens/HomeScreen.tsx
+++ client/budgetmind-client/src/screens/HomeScreen.tsx
@@ -18,11 +18,87 @@ export default function HomeScreen() {
     console.log(authILoginLog)
     return (
         <MainDiv>
-            <h3>
-                welcome {authILoginLog?.user}
-            </h3>
+            <GeneralDiv>
+                <ListDiv>
+                    <ListRowDiv>
+                        <ConceptH6>
+                            concept
+                        </ConceptH6>
+                        <AmountH6  theme={"green"}>
+                            +500
+                        </AmountH6>
+                    </ListRowDiv>
+                    <ListRowDiv>
+
+                        <ConceptH6>
+                            concept
+                        </ConceptH6>
+                        <AmountH6 theme={'red'}>
+                            -500
+                        </AmountH6>
+                    </ListRowDiv>
+
+                </ListDiv>
+
+                <WelcomeDiv>
+
+                    <WelcomeH5>
+                        welcome {authILoginLog?.user}
+                    </WelcomeH5>
+                </WelcomeDiv>
+                <CreateDiv>
+                <CreateInput type="text"/>
+                <CreateInput type="number"/>
+                </CreateDiv>
+            </GeneralDiv>
         </MainDiv>
     )
 }
+const GeneralDiv = styled.div`
+ display:flex;
+ justify-content:space-between;
+ width:100%;
+ //flex-direction:row;
+`
 
+const ListDiv = styled.div`
+ border:1px solid black;
+ padding:10px;
+ display:flex;
+flex-direction:column;
+`
+const ListRowDiv = styled.div`
+ border:1px solid black;
+ padding:10px;
+ display:flex;
+ flex-direction:row;
+`
 
+const ConceptH6 = styled.p`
+    color:#333;
+    padding:0 10px;
+    font-size:18px;
+    margin:2px;
+    `
+const AmountH6 = styled.p`
+    color:${props => props.theme};
+    padding:0 10px;
+    font-size:18px;
+    margin:2px;
+`
+const WelcomeDiv = styled.div`
+ padding:10px;
+ border:1px solid black;
+`
+
+const WelcomeH5 = styled.h5`
+    color:green;
+    font-size:25px;
+`
+const CreateDiv = styled.div `
+border:1px solid black;
+padding:10px;
+`
+const CreateInput = styled.input `
+
+`
